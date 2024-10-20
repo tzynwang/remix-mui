2b5e5e19dd1cd95e8d81e3be77a0e2518b9afa8a
Tzu Yin <tzyn.wang@gmail.com>
2024-10-20 18:36:58 +0800

    [feat] update vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,17 @@
-import { vitePlugin as remix } from "@remix-run/dev";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { vitePlugin as remix } from '@remix-run/dev';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
-declare module "@remix-run/node" {
+declare module '@remix-run/node' {
   interface Future {
     v3_singleFetch: true;
   }
 }
 
-export default defineConfig({
+export default defineConfig(({ isSsrBuild }) => ({
+  ssr: {
+    noExternal: isSsrBuild ? [/^@mui\/*/] : undefined, // https://vite.dev/config/ssr-options.html#ssr-noexternal
+  },
   plugins: [
     remix({
       future: {
@@ -21,4 +24,4 @@ export default defineConfig({
     }),
     tsconfigPaths(),
   ],
-});
+}));
